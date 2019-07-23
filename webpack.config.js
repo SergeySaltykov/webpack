bc98009a7e7f000828f1c993a9cd529316307188
s.saltykov <s.saltykov@dev.userstory.ru>
2019-07-23 14:21:50 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,20 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
 const paths = {
-  build: path.resolve(__dirname, './www/build'),
-  src: path.resolve(__dirname, './src'),
-  www: path.resolve(__dirname, './www'),
+    build: path.resolve(__dirname, './www/build'),
+    src: path.resolve(__dirname, './src'),
+    www: path.resolve(__dirname, './www'),
 };
 const defaultPort = 8000;
 const port = process.env.PORT || defaultPort;
 const isProd = process.env.NODE_ENV === 'production';
 const stats = {
-  colors: true,
-  errorDetails: true,
+    colors: true,
+    errorDetails: true,
 };
 const cleanOptions = {
-  dry: false,
-  verbose: true,
+    dry: false,
+    verbose: true,
 };
 
 // style loaders
@@ -50,35 +50,35 @@ const cleanOptions = {
 
 // plugins
 const plugins = [
-        new HtmlWebpackPlugin({
-            alwaysWriteToDisk: true, // генерация записи на диск для локального запуска
-            inject: true, // все скрипты будут грузиться после body
-            hash: true, // добавляет в конце файла хеш чтобы не кешировались стили
-            template: path.resolve(paths.src, './index.html'), // путь к шаблону
-            filename: path.resolve(paths.www, './index.html'), // путь к файлу
-            title: 'Application',
-        }),
-        new MiniCssExtractPlugin({ // извлекает css в отдельные файлы
-            chunkFilename: 'css/[id].min.css',
-            filename: 'css/[name].min.css',
-        }),
-        new HtmlWebpackHarddiskPlugin(),  // плагин позволяет убрать пути по умолчанию
-    ]
-;
+    new HtmlWebpackPlugin({
+        alwaysWriteToDisk: true, // генерация записи на диск для локального запуска
+        inject: true, // все скрипты будут грузиться после body
+        hash: true, // добавляет в конце файла хеш чтобы не кешировались стили
+        template: path.resolve(paths.src, './index.html'), // путь к шаблону
+        filename: path.resolve(paths.www, './index.html'), // путь к файлу
+        title: 'Application',
+    }),
+    new MiniCssExtractPlugin({ // извлекает css в отдельные файлы
+        chunkFilename: 'css/[id].min.css',
+        filename: 'css/[name].min.css',
+    }),
+    new HtmlWebpackHarddiskPlugin(), // плагин позволяет убрать пути по умолчанию
+];
 const styleLoaderOrPlugin = isProd ? MiniCssExtractPlugin.loader : 'style-loader'; // MiniCssExtractPlugin used only on production, for hot reload
 
 if (isProd) {
     plugins.unshift(
-        new CleanWebpackPlugin(cleanOptions)); //порядок важен, сначала идет отчистка билдов
+        new CleanWebpackPlugin(cleanOptions),
+    ); // порядок важен, сначала идет отчистка билдов
 } else {
-  plugins.push(new webpack.HotModuleReplacementPlugin()); // обновление модуля без перезагрузки при изменении кода
+    plugins.push(new webpack.HotModuleReplacementPlugin()); // обновление модуля без перезагрузки при изменении кода
 }
 
 /* minimization сss & js */
 const minimizer = [
     new UglifyJsPlugin({
         cache: true, // кэш
-        parallel: true, //использует параллельный запуск для провышени скорости сборки
+        parallel: true, // использует параллельный запуск для провышени скорости сборки
         sourceMap: true, // поволяет показывать правильные пути модулей при ошибках (замедляет компиляцию)
     }),
     new OptimizeCssAssetsPlugin({
@@ -101,7 +101,7 @@ module.exports = {
         contentBase: paths.www,
         historyApiFallback: true, // fallBack for API
         inline: true,
-        port: port,
+        port,
         stats,
     },
     devtool: isProd ? 'source-map' : 'inline-source-map',
@@ -110,16 +110,16 @@ module.exports = {
             '@babel/polyfill', // кроссбраузерность
             'whatwg-fetch',
         ],
-        index: './src/app/index',    // определяем точку входа
+        index: './src/app/index', // определяем точку входа
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,        // определяем тип файлов для преобразования
-                exclude: /node_modules/,    // исключаем из обработки папку node_modules
+                test: /\.(js|jsx)$/, // определяем тип файлов для преобразования
+                exclude: /node_modules/, // исключаем из обработки папку node_modules
                 use: {
-                    loader: 'babel-loader'  // определяем загрузчик для транспиляции
-                }
+                    loader: 'babel-loader', // определяем загрузчик для транспиляции
+                },
             },
             {
                 test: /\.less$/,
@@ -144,20 +144,20 @@ module.exports = {
                     name: 'build/fonts/[hash:8].[ext]?[hash:4]',
                 },
             },
-        ]
+        ],
     },
     optimization: {
-        minimizer: minimizer, // по дефолту включен в прод режиме
+        minimizer, // по дефолту включен в прод режиме
         splitChunks: {
-            automaticNameDelimiter: '-', //разделитель чанков,
-            chunks: 'all'
+            automaticNameDelimiter: '-', // разделитель чанков,
+            chunks: 'all',
         },
     },
-    output: {      // выход билда
+    output: { // выход билда
         chunkFilename: 'js/[name].min.js',
         filename: 'js/[name].min.js',
         path: paths.build,
-        publicPath: '/build'  // важный параметр, так же webpack-dev-server использует для определения где следует обслуживать выходные файлы
+        publicPath: '/build', // важный параметр, так же webpack-dev-server использует для определения где следует обслуживать выходные файлы
     },
     resolve: { // расширение модулей
         extensions: ['.js', '.jsx'], // определяем расширение файла (значения по умолчанию webpack ['.wasm', '.mjs', '.js', '.json'])
@@ -166,5 +166,5 @@ module.exports = {
             'node_modules',
         ],
     },
-    plugins: plugins,
+    plugins,
 };
